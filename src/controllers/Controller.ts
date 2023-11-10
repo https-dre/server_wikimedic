fe87bce1c76360fc5403fc2ae45d309d3d68ecb1
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-10 19:40:33 +0000

    bug: database

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -9,6 +9,7 @@ const getAllUsers = async (__req : Request, res : Response) => { // rota exclusi
   catch (err)
   {
     console.log(err)
+    return res.status(500).json({message:"Erro Interno no Servidor"})
     throw err
   }
 }
