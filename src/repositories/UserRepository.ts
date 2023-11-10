bdbdc283c1b51b58b9e341b8cd454cb448f1eafa
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-10 19:33:35 +0000

    change database.db config

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,7 +2,7 @@ import { Database } from "sqlite3"
 import { v4 as uuidv4 } from 'uuid';
 import hashPassword from "../crypt/crypt"
 import { User } from "../models/User"
-import { ResponseHttp } from "../models/ResponseHttp"
+import { IResponseHttp as ResponseHttp } from "../models/ResponseHttp"
 
 
 interface IUserRepositoryInterface {
