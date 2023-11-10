1b7438c68edc6b950d4f4030a1603919533e7a06
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-10 15:00:02 +0000

    add folders and files

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,117 @@
+import { Database } from "sqlite3"
+import { v4 as uuidv4 } from 'uuid';
+import hashPassword from "../crypt/crypt"
+import { User } from "../models/User"
+import { ResponseHttp } from "../models/ResponseHttp"
+
+
+interface IUserRepositoryInterface {
+  postUser(user : User): Promise<ResponseHttp>;
+  findByEmail(email : string) : Promise<ResponseHttp>;
+}
+
+
+export class UserRepository implements IUserRepositoryInterface {
+
+  private db : Database
+
+  constructor(dbpath : string) {
+    this.db = new Database(dbpath);
+    //console.log("IUserRepository initialized")
+  }
+
+  async findByEmail(email: string): Promise<ResponseHttp> {
+    return new  Promise((resolve, reject) => {
+      const query = `SELECT * FROM users WHERE email = ?`
+      this.db.get(query, [email], (err, rows: any) => {
+        if (err) {
+          reject(err)
+        }
+        else {
+          if(rows)
+          {
+            if(rows.lenght == 1)
+              {
+                const user: User = {
+                  id: rows[0].id,
+                  name: rows[0].name,
+                  email: rows[0].email,
+                  email_reserva: rows[0].email_reserva,
+                  password: rows[0].password
+                }
+                const httpResponse : ResponseHttp = {
+                  body: user,
+                  status: 200
+                }
+
+                resolve(httpResponse)
+              }
+              else {
+                const httpResponse : ResponseHttp = {
+                  body: false, // se o usuário não existir, ele retorna o body como falso
+                  status: 404
+                }
+                resolve(httpResponse)
+              }
+          }
+          else
+          {
+            reject(new Error("User not found"))
+          }
+        }
+      })
+    })
+  }
+
+  async postUser(user : User): Promise<ResponseHttp> {
+    try {
+
+      const findByEmailResponse = await this.findByEmail(user.email)
+      
+      if(!findByEmailResponse.body) // se o email não existir
+      {
+        //const userFinded = findByEmailResponse.body
+        // Criando um hash seguro da senha
+          const hashedPassword = await hashPassword(user.password);
+         /*  user = {
+            id: uuidv4(),
+            password: hashedPassword
+          } */
+          const formatedUser : User = {
+            id: uuidv4(),
+            name: user.name,
+            email: user.email,
+            email_reserva: user.email_reserva,
+            password: hashedPassword
+          }
+
+          this.db.serialize(() => {
+            const query = `INSERT INTO users (id, name, email, email_reserva, password) VALUES('${formatedUser.id}', '${formatedUser.name}', '${formatedUser.email}', '${formatedUser.email_reserva}', '${formatedUser.password}')`;
+            this.db.run(query);
+          });
+
+          //this.db.close();
+
+          const response : ResponseHttp = {
+            body: formatedUser,
+            status: 201
+          }
+        return response
+      }
+      else { // caso o email exista
+        const httpResponse : ResponseHttp = {
+          body: {message: "Este email já existe"},
+          status: 404
+        }
+        return httpResponse
+      }
+    } 
+    catch (err) {
+      console.log(err);
+      throw err; 
+    }
+  }
+  
+  
+  
+}
---
+++ src/routers/Router.ts
@@ -0,0 +1,32 @@
+import express from "express"
+
+import Controller from "../controllers/Controller"
+import { UserController } from "../controllers/UserController"
+import { UserRepository } from "../repositories/UserRepository"
+
+const Router = express.Router()
+
+Router.get('/', (__req, res)=>{
+  res.send("<h1>httpServer to Wikimedic</h1>")
+})
+
+Router.get('/getAllUsers', Controller.getAllUsers)
+
+Router.post('/sign', async (req,res) => {
+  const userRepository = new UserRepository('./src/data/data.db')
+
+  const userController = new UserController(userRepository)
+  
+  try {
+    const response = await userController.postUser(req)
+    res.status(response.status).json(response.body)
+  }
+  catch (err)
+  {
+    res.status(500).json({message: "Erro interno no servidor"})
+  }
+})
+
+
+
+export default Router
