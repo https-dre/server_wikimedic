c2d85a7765fc27afd8cfcb14f56612f20a5c7a19
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-15 21:58:15 -0300

    Atualização do import

--- src/repositories/protocols/IUserRepository.ts
+++ src/repositories/protocols/IUserRepository.ts
@@ -1,9 +1,9 @@
-import { User } from "../models/User"
+import { User } from "../../models/User"
 
 export interface IUserRepository {
     postUser(user: User): Promise<User>;
-    //deleteUser(id : string) : Promise<User>;
-    findByEmail(email: string): Promise<User | false>;
-    findById(id  : string):Promise<User | false>
+    deleteUser(id : string) : Promise<void>;
+    findByEmail(email: string): Promise<User | null>;
+    findById(id  : string):Promise<User | null>
     getAllUsers(): Promise<User[]>;
 }
