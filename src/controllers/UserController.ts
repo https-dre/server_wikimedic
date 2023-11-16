62f0f1564b398e3ee918286dfc7b9ab528d61bb5
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-15 21:57:18 -0300

    Validação da Rota deleteUser

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,13 +3,12 @@ import { User } from "../models/User"
 import { Request, Response } from "express"
 
 import { IResponseHttp as ResponseHttp } from "../models/ResponseHttp"
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
 import { v4 as uuidv4 } from 'uuid';
+import hashPassword from "../crypt/crypt";
 
 interface IUserController {
   postUser(req: Request, res: Response): Promise<void>;
-  //deleteUser(req: Request, res : Response): Promise<void>;
+  deleteUser(req: Request, res : Response): Promise<void>;
   getAllUsers(req : Request, res : Response): Promise<void>;
 }
 
@@ -24,18 +23,20 @@ export class UserController implements IUserController {
   }
 
   async postUser(req: Request, res: Response) {
+
+    
     const user: User = {
       id: uuidv4(),
       name: req.body.name,
       email: req.body.email,
       email_reserva: req.body.email_reserva,
-      password: req.body.password
+      password: await hashPassword(req.body.password)
     }
 
     try {
       //valida se o email já existe
       const userFinded = await this.userRepository.findByEmail(user.email)
-      if (!userFinded) // se o usuário não existir, será false
+      if (userFinded == null) // se o usuário não existir, será nullo
       {
         const userCreated = await this.userRepository.postUser(user)
         const response: ResponseHttp = {
@@ -58,35 +59,35 @@ export class UserController implements IUserController {
       throw err
     }
   }
-  /* async deleteUser(req : Request, res : Response): Promise<void> {
+  async deleteUser(req : Request, res : Response): Promise<void> {
       try {
-        const id = req.params.id
-        const userFinded = await this.userRepository.findById(id)
-        if(!userFinded)
+        console.log(req.params.id)
+        if(req.params.id)
         {
-          res.status(404).json({message: "O usuário não existe"})
-        }
-        else
-        {
-          const userDeleted = await this.userRepository.deleteUser(userFinded.id)
-          const response = {
-            id : userDeleted.id,
-            name : userDeleted.name,
-            email : userDeleted.email,
-            email_reserva : userDeleted.email_reserva,
-            password : userDeleted.password,
-            deleted : true
+          const id = req.params.id
+          const userFinded = await this.userRepository.findById(id)
+          console.log(userFinded)
+          if(userFinded)
+          {
+            await this.userRepository.deleteUser(userFinded.id)
+            res.status(200).json({message : "Usuário deletado!"})
+            
           }
-          res.status(200).json({response})
+          else
+          {
+            res.status(404).json({message: "O usuário não existe"})
+          }
         }
-        
+        else {
+          res.status(400).json({message : "Informe o id de Usuário"})
+        }
       }
       catch (err)
       {
         console.log(err)
         res.status(500).json({message: "Erro Interno no servidor"})
       }
-  } */
+  }
   async getAllUsers(req: Request, res: Response): Promise<void> {
       try {
         const users = await this.userRepository.getAllUsers()
