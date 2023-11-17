324e095ef53f5ecf5bb0892fdc6e81694b3490ee
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-17 16:10:48 -0300

    Delete Favorito adicionado

--- src/repositories/mongo/FavoritoRepository.ts
+++ src/repositories/mongo/FavoritoRepository.ts
@@ -35,11 +35,37 @@ export class FavoritoRepository implements IFavoritoRepository
     {
         try {
             const FavoritoCollection = mongo.db.collection('Favorito')
-            const docs = await FavoritoCollection.find({_id : id as unknown as ObjectId}).toArray()
+            const docs = await FavoritoCollection.find({idUser : id as unknown as ObjectId}).toArray()
+    
             if(docs.length > 0)
             {
                 const favs = docs.map(doc => toFavorito(doc))
                 return favs
+            }
+            else
+            {
+                return null
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+    async delete(id: string): Promise<void> {
+        try {
+            const FavoritoCollection = mongo.db.collection('Favorito')
+            await FavoritoCollection.deleteOne({_id : id as unknown as ObjectId})
+        } catch (error) {
+            throw error
+        }
+    }
+    async findById(Id: string): Promise<Favorito | null> {
+        try {
+            const FavoritoCollection = mongo.db.collection('Favorito')
+            const doc = await FavoritoCollection.findOne({ _id : Id as unknown as ObjectId})
+            if(doc != null)
+            {
+                const fav = toFavorito(doc)
+                return fav
             }
             else
             {
