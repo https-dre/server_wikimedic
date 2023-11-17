4dd8b9004efe338c454293a983e42a64e9e5e858
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-17 17:22:23 -0300

    Variáveis de ambiente adicionadas

--- src/data/mongoDB/conn.ts
+++ src/data/mongoDB/conn.ts
@@ -1,6 +1,9 @@
 // WvEyLSoepVJz6KbU
 //mongodb+srv://root:WvEyLSoepVJz6KbU@cluster0.ioeey37.mongodb.net/
 import { MongoClient, Db } from "mongodb"
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 export const mongo = {
   client : undefined as unknown as MongoClient,
@@ -8,14 +11,21 @@ export const mongo = {
 
   async conn(): Promise<void> {
     try {
-      const client = new MongoClient('mongodb+srv://root:WvEyLSoepVJz6KbU@cluster0.ioeey37.mongodb.net/');
-      this.client = client;
-      await client.connect();
-      console.log('MongoDB connected')
+      const url = process.env.MONGO_URL
+      if (!url) {
+        throw new Error("A variável de ambiente MONGO_URL não está definida.");
+      }
+      else
+      {
+        const client = new MongoClient(url as unknown as string);
+        this.client = client;
+        await client.connect();
+        console.log('MongoDB connected')
 
-      const db = client.db('cluster0');
-      this.db = db;
-      console.log('Db client created and mongoDB connected')
+        const db = client.db('cluster0');
+        this.db = db;
+        console.log('Db client created and mongoDB connected')
+      }
     } 
     catch (error) {
       console.error("Error connecting to MongoDB", error);
