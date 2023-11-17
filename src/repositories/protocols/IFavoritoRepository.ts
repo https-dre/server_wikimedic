1fea331c971602cb6167b90ac8a12fc28f6eaf8c
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-17 16:11:08 -0300

    FindById and delete adicionado a interface

--- src/repositories/protocols/IFavoritoRepository.ts
+++ src/repositories/protocols/IFavoritoRepository.ts
@@ -2,6 +2,8 @@ import { Favorito } from "../../models/Favorito";
 
 export interface IFavoritoRepository {
     postFav(fav : Favorito): Promise<Favorito>;
+    findById( Id : string) : Promise<Favorito | null>;
     findByIdUser(id : string): Promise<Favorito[] | null>;
     //findOneByIdUser(id : string) : Promise <Favorito | null>;
+    delete( id : string) : Promise<void>;
 }
