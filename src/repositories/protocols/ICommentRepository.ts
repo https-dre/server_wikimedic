0fe397c9d636aeb92aa3287035c23444e874a18c
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-21 11:06:51 +0000

    Protocol ICommentRepository adicionado

--- src/repositories/protocols/ICommentRepository.ts
+++ src/repositories/protocols/ICommentRepository.ts
@@ -2,5 +2,6 @@ import { Comment } from "../../models/Comment"
 
 export interface ICommentRepository
 {
-    
+    postComment(comment : Comment): Promise<Comment>;
+    findByIdUser(Id : string): Promise<Comment[]>;
 }
