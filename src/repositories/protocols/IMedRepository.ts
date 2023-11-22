72048d395273e56be33456180f267246ad3de871
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-22 14:45:59 +0000

    Add update Medic

--- src/repositories/protocols/IMedRepository.ts
+++ src/repositories/protocols/IMedRepository.ts
@@ -3,6 +3,7 @@ import { Medicamento } from "../../models/Medicamento";
 export interface IMedRepository {
     findByNumProcess(numProcesso : string) : Promise<Medicamento | null>;
     postMed(med : Medicamento) : Promise<Medicamento>;
+    //updateById(med : Medicamento) : Promsise<Medicamento>;
     findById(id : string): Promise<Medicamento | null>;
     getAll() : Promise<Medicamento[]>;
     delete( Id : string) : Promise<void>;
