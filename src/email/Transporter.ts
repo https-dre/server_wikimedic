6f2c0bb4e795305c3df96cb7e41dceb7a62505da
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-26 16:38:42 -0300

    Correção no transporter

--- src/email/Transporter.ts
+++ src/email/Transporter.ts
@@ -5,32 +5,31 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 //console.log(process.env.EMAIL,process.env.EMAIL_PASSWORD)
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: 'smtp.office365.com',
   port : 587,
   secure: false,
   auth: {
-   user: 'wikimedic_no_reply@hotmail.com',
-   pass: 'aabeg2023'
+   user: process.env.EMAIL,
+   pass: process.env.EMAIL_PASSWORD
   }
-  });
+});
 
-const to = "deoliveiradiasandre@gmail.com"
 
-var mailOptions = {
+/* var mailOptions = {
     from : "wikimedic_no_reply@hotmail.com",
-    to : to,
+    to : ,
     subject : 'Test Email',
     text : "Email de Teste da Wikimedic"
 }
 
 transporter.sendMail(mailOptions, function(error, info){
-    /* if(error)
+    if(error)
     {
         console.log(error)
     }
     else
     {
         console.log(info)
-    } */
-})
+    }
+}) */
