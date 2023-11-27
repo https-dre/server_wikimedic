1b38ee0b5874fda09e3fd6a297e3b816b63e768e
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-27 18:47:50 -0300

    Retirando cors

--- Server.ts
+++ Server.ts
@@ -18,7 +18,7 @@ const main = async () => {
     await ClientMongo.conn() // conectando ao banco de dados antes de iniciar a aplicação
     app.use(express.json())
     //app.use(Middle) //Middleware é um filtro de Acesso para o Servidor 
-    app.use(cors({ origin: "*" })) // permitindo qualquer origem se conectar ao ao Servidor
+    //app.use(cors({ origin: "*" })) // permitindo qualquer origem se conectar ao ao Servidor
 
     app.use(Router) // colocando o arquivo ./src/routers/Router.ts para gerenciar as rotas da aplicação
 
