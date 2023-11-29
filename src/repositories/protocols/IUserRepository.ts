dd1e1993b27a541b72e5e39ea7880513df5acac2
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-29 15:01:13 -0300

    added findByEmail_Reserva

--- src/repositories/protocols/IUserRepository.ts
+++ src/repositories/protocols/IUserRepository.ts
@@ -4,6 +4,7 @@ export interface IUserRepository {
     save(user: User): Promise<User>;
     deleteUser(id : string) : Promise<void>;
     findByEmail(email: string): Promise<User | null>;
+    findByEmailReserva(email : string) : Promise<User | null>;
     findById(id  : string):Promise<User | null>
     getAllUsers(): Promise<User[]>;
     updateUser(newUser : any, idUser : string) : Promise<User>;
