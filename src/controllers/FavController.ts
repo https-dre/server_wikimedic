0e36141507699d6d5a2f567c752189e6de8e0a72
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-30 11:08:57 -0300

    Validação e Autenticação de Usuário adicionada

--- src/controllers/FavController.ts
+++ src/controllers/FavController.ts
@@ -4,38 +4,28 @@ import { v4 as uuidv4 } from 'uuid';
 import { Favorito } from '../models/Favorito';
 
 import { IUserRepository } from '../repositories/protocols/IUserRepository';
-import { IMedRepository as IMedicamentoRepository } from '../repositories/protocols/IMedRepository';
+import { IMedRepository, IMedRepository as IMedicamentoRepository } from '../repositories/protocols/IMedRepository';
 import { IFavoritoRepository } from '../repositories/protocols/IFavoritoRepository';
 import { MedicamentoRepository } from '../repositories/mongo/MedicamentoRepository';
+import { User } from '../models/User';
 
 
 export class FavController
 {
     favRepository : IFavoritoRepository
-    userRepository? : IUserRepository
-    medRepository? : IMedicamentoRepository
     
     constructor(rep : IFavoritoRepository, repUser? : IUserRepository, md? : IMedicamentoRepository)
     {
         this.favRepository = rep
-        if(repUser)
-        {
-            this.userRepository = repUser
-        }
-        if(md)
-        {
-            this.medRepository = md
-        }
     }
 
-    async save(req: Request, res: Response): Promise<void> {
+    async save(req: Request, res: Response, userRepo : IUserRepository, medRepo : IMedRepository): Promise<void> {
 
-        if( this.userRepository != null && this.medRepository != null)
-        {
+
             try
             {
-                const userFinded = await this.userRepository.findById(req.body.idUser)
-                const medFinded = await this.medRepository.findByNumProcess(req.body.numProcesso)
+                const userFinded = await userRepo.findByEmail(req.body.email)
+                const medFinded = await medRepo.findByNumProcess(req.body.numProcesso)
                 //console.log(userFinded)
                 //console.log(medFinded)
                 if(userFinded != null && medFinded != null)
@@ -73,7 +63,6 @@ export class FavController
             {
                 res.status(500).json("Erro Interno no Servidor, aguarde ou contate o administrador")
             }
-        }
         
     }
 
@@ -88,7 +77,7 @@ export class FavController
 
         }
     }
-    async findByIdUser(req : Request, res : Response) : Promise<void>
+    async findByIdUser(req : Request, res : Response, medRepo : IMedRepository) : Promise<void>
     {
         try {
             if(req.params.id)
@@ -96,7 +85,27 @@ export class FavController
                 const favs = await this.favRepository.findByIdUser(req.params.id)
                 if(favs != null)
                 {
-                    res.status(200).json(favs)
+                    const result = await Promise.all(
+                        favs.map(async (fav) =>{
+                            const med = await medRepo.findById(fav.idMed)
+                            if(med)
+                            {
+                                return {
+                                    medic :{
+                                        id : fav.idMed,
+                                        name : med.name,
+                                        numProcesso : med.numProcesso
+                                    },
+                                    idUser : fav.idUser
+                                }
+                            }
+                            else
+                            {
+                                return "Medicamento Deletado out Not Found"
+                            }
+                        })
+                    );
+                    res.status(200).json(result)
                 }
                 else
                 {
@@ -111,28 +120,40 @@ export class FavController
             res.status(500).json("Erro Interno no Servidor")
         }
     }
-    async delete(req: Request, res: Response): Promise<void> {
+    async delete(req: Request, res: Response, userRepository : IUserRepository): Promise<void> {
         try {
             if(req.params.id)
             {
-                const doc = await this.favRepository.findById(req.params.id)
-                if(doc != null)
-                {
-                    await this.favRepository.delete(doc.id)
-                    res.status(200).json( "Favorito deletado")
-                }
-                else
-                {
-                    res.status(404).json( "Favorito not found")
-                }
+                const user = await userRepository.findByEmail(req.body.auth.email)
+                const fav = await this.favRepository.findById(req.params.id)
 
+                if(user != null && fav != null)
+                {
+                    if(user.id == fav.idUser)
+                    {
+                        await this.favRepository.delete(fav.id)
+                        res.status(200).json( "Favorito deletado")
+                    }
+                    else
+                    {
+                        res.status(404).json( "Favorito not found")
+                    }
+                }
+                else if(user == null)
+                {
+                    res.status(404).json("User Not Found")
+                }
+                else if(fav == null)
+                {
+                    res.status(404).json("Favorito Not Found")
+                }
             }
             else
             {
                 res.status(400).json( "Informe um id nos parâmetros da Requisição")
             }
         } catch (error) {
-            
+            throw error
         }
     }
     async getAll(req: Request, res: Response): Promise<void> {
