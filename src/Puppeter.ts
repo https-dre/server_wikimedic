f30acb08b0a8d222cb91e0b27b569b7b15bed19d
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-06 14:25:24 +0000

    test: src/Puppeter.ts; Bot sem GUI;

--- src/Puppeter.ts
+++ src/Puppeter.ts
@@ -1,7 +1,16 @@
 import * as puppeteer from "puppeteer";
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+    "--disable-gpu",
+    "--disable-dev-shm-usage",
+    "--disable-setuid-sandbox",
+    "--no-sandbox"
+  ]
+  });
+
   const page = await browser.newPage();
 
   await page.goto('https://consultaremedios.com.br/');
