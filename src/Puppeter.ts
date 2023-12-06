389c997c6693a004b8176e2540725f1055533d0d
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-05 21:43:00 -0300

    fix: acessando página do medicamento

--- src/Puppeter.ts
+++ src/Puppeter.ts
@@ -1,44 +1,52 @@
-import * as puppeteer from "puppeteer"
+import * as puppeteer from "puppeteer";
 
 async function main() {
-    const browser = await puppeteer.launch({ headless : false})
-    const page = await browser.newPage()
-    await page.goto('https://consultaremedios.com.br/')
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
+  await page.goto('https://consultaremedios.com.br/');
 
-    const medic = "Buscopan"
-    await page.click('.BaseInput__medium_0')
-    await page.type('.BaseInput__medium_0', medic)
-    await page.screenshot({ path : './outDir/puppeteer/consulta.png'})
+  const medic = "Buscopan";
 
-    const resultados = await page.evaluate(()=>{
-        const suggestionTitles = document.querySelectorAll('.SearchAutocomplete__suggestionTitle_0')
-        const medic = "Buscopan"
-        for (let i = 0; i < suggestionTitles.length; i++) {
-            if(suggestionTitles[i].innerHTML.includes(medic))
-            {
-                suggestionTitles[i].setAttribute('id', 'alvo') 
+  // Espera até que o campo de busca esteja visível antes de interagir
+  await page.waitForSelector('.BaseInput__medium_0');
+  await page.click('.BaseInput__medium_0');
+  await page.type('.BaseInput__medium_0', medic);
+  await page.screenshot({ path: './outDir/puppeteer/consulta.png' });
 
-                return true
-            }
-        }
-    })
+  const resultados = await page.evaluate((medic) => {
+    const suggestionTitles = document.querySelectorAll('.SearchAutocomplete__suggestionTitle_0');
 
-    console.log(resultados)
-    if(resultados == true)
-    {
-        page.click('#alvo')
-        const indicacao = await page.evaluate(()=>{
-            const panel_body = document.querySelector('.panel-body')
-            if(panel_body)
-            {
-                return panel_body.children[0]
-            }
-        })
-        console.log(indicacao)
+    for (let i = 0; i < suggestionTitles.length; i++) {
+      if (suggestionTitles[i].innerHTML.includes(medic)) {
+        suggestionTitles[i].setAttribute('id', 'alvo');
+        return true;
+      }
     }
 
-    
+    return false;
+  }, medic);
+
+  console.log(resultados);
+
+  
+    await page.waitForSelector('#alvo');
+    await page.click('#alvo');
+
+
+    console.log("Indicação")
+    const indicacao = await page.evaluate(() => {
+      const panel_body = document.querySelector('.panel-body') as HTMLElement | null;
+      if (panel_body) {
+        return panel_body.children[0].textContent?.trim() || null;
+      }
+      return null;
+    });
+
+    console.log(indicacao);
+  
+
+  
 }
 
-main()
+main();
