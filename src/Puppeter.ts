41b768b26f93e6fb591ca9e07d7a961b104f6e91
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-07 19:09:31 -0300

    feat: Puppeter.ts; Class BotService.

--- src/Puppeter.ts
+++ src/Puppeter.ts
@@ -1,57 +1,113 @@
 import * as puppeteer from "puppeteer";
 
-async function main() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--no-sandbox"
-    ]
-  });
 
-  const page = await browser.newPage();
+export class BotService {
+  browser: puppeteer.Browser;
 
-  await page.goto('https://consultaremedios.com.br/');
+  constructor(headless: boolean) {
+    this.browser = null as any;
+    this.initializeBrowser(headless)
+    .then( () => console.log('alalala'))
+    .catch( () => new Error('Erro na Criação puppeteer.Browser'))
+  }
 
-  const medic = "Buscopan";
+  async initializeBrowser(headless: boolean): Promise<void> {
+    this.browser = await puppeteer.launch({
+      headless: headless,
+      args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-sandbox"
+      ]
+    });
+  }
 
-  // Espera até que o campo de busca esteja visível antes de interagir
-  await page.waitForSelector('.BaseInput__medium_0');
-  await page.click('.BaseInput__medium_0');
-  await page.type('.BaseInput__medium_0', medic);
-  //await page.screenshot({ path: './outDir/puppeteer/consulta.png' });
+  async newPage(): Promise<puppeteer.Page> {
+      return this.browser.newPage();
+  }
 
-  const resultados = await page.evaluate((medic) => {
-    const suggestionTitles = document.querySelectorAll('.SearchAutocomplete__suggestionTitle_0');
+  async closeBrowser(): Promise<void> {
+    // Fecha o navegador quando você terminar de usá-lo
+    if (this.browser) {
+      await this.browser.close();
+    }
+  }
 
-    for (let i = 0; i < suggestionTitles.length; i++) {
-      if (suggestionTitles[i].innerHTML.includes(medic)) {
-        suggestionTitles[i].setAttribute('id', 'alvo');
-        return true;
+  async searchMedic(medic : string) : Promise<any>
+  {
+    if (!this.browser) {
+      throw new Error("Navegador não inicializado. Abortando pesquisa do medicamento.");
+    }
+
+    const page = await this.browser.newPage()
+
+    await page.goto('https://consultaremedios.com.br/');
+    // Espera até que o campo de busca esteja visível antes de interagir
+    await page.waitForSelector('.BaseInput__medium_0');
+    await page.click('.BaseInput__medium_0');
+    await page.type('.BaseInput__medium_0', medic);
+    
+
+    await page.waitForSelector('.SearchAutocomplete__suggestionTitle_0')
+
+    const resultados = await page.evaluate((medic) => {
+      const suggestionTitles = document.querySelectorAll('.SearchAutocomplete__suggestionTitle_0');
+
+      for (let i = 0; i < suggestionTitles.length; i++) {
+        console.log(suggestionTitles[i])
+        if (suggestionTitles[i].innerHTML.includes(medic)) {
+          suggestionTitles[i].setAttribute('id', 'alvo');
+          
+          return suggestionTitles[i].innerHTML;
+        }
       }
+
+      return false;
+    }, medic);
+
+
+    console.log(resultados);
+    if(resultados != false)
+    {
+      console.log('Selecionando Resultado Medicamento')
+      await page.waitForSelector('#alvo');
+      await page.click('#alvo');
+
+      console.log('GET Contraindicação')
+      await page.waitForSelector('.js-scroll-to')
+      await page.click('.js-scroll-to')
+      await page.waitForSelector('.panel-body')
+
+      const indicacao = await page.evaluate(() => {
+        const textContent = document.querySelectorAll('.text-content') as NodeListOf<HTMLElement>;
+        let result = [];
+        for (let i = 0; i < textContent.length; i++) {
+          result.push(textContent[i].textContent);
+          console.log(textContent[i].textContent)
+        }
+        return result
+      });
+
+      console.log('\nIndicação')
+      console.log(indicacao[0])
+      console.log('\nContraindicação')
+      console.log(indicacao[1])
+      await this.closeBrowser()
+      return indicacao
     }
-
-    return false;
-  }, medic);
-
-  console.log(resultados);
-
-  console.log('Selecionando Resultado Medicamento')
-  await page.waitForSelector('#alvo');
-  await page.click('#alvo');
-
-  console.log('GET Contraindicação')
-  const indicacao = await page.evaluate(() => {
-    const panel_body = document.querySelector('.panel-body') as HTMLElement | null;
-    if (panel_body) {
-      return panel_body.children[0].textContent
+    else
+    {
+      console.log('Erro na pesquisa do Medicamento')
+      await this.closeBrowser()
+      return null
     }
-    return null;
-  });
-
-  console.log(indicacao);
+  }
 }
 
-main();
+async function run() {
+  const botService = new BotService(false);
+  await botService.searchMedic('Paracetamol');
+}
+
+run()
