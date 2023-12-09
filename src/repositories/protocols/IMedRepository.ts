12e45874689a5e648a3ec4c9edc2bcefa3516281
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-09 16:25:11 -0300

    feat (IMedRepository.ts): Adiciona UpdateMedic.

--- src/repositories/protocols/IMedRepository.ts
+++ src/repositories/protocols/IMedRepository.ts
@@ -3,7 +3,7 @@ import { Medicamento } from "../../models/Medicamento";
 export interface IMedRepository {
     findByNumRegistro(registro : string) : Promise<Medicamento | null>;
     save(med : Medicamento) : Promise<Medicamento>;
-    //updateById(med : Medicamento) : Promsise<Medicamento>;
+    updateByNumRegistro(med : any, num : string) : Promise<any>;
     findById(id : string): Promise<Medicamento | null>;
     getAll() : Promise<Medicamento[]>;
     delete( Id : string) : Promise<void>;
