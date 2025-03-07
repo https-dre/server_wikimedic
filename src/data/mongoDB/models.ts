ed1d1220ea3f2a14e912e6cb04c4c546d2a2a6de
https-dre <diaso.andre@outlook.com>
2025-03-07 19:49:43 -0300

    remove unused files

--- src/data/mongoDB/models.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-import { UserSchema, MedicamentoSchema, FavoritoSchema} from "./schemas"
-
-export const UserModel = mongoose.model("User", UserSchema)
-
-export const MedicamentoModel = mongoose.model("Medicamento", MedicamentoSchema)
-
-export const FavoritoModel = mongoose.model("Favorito", FavoritoSchema)
