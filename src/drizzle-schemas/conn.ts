97b08a552e0c7f93703481a353c6ba52c8439cb5
https-dre <diaso.andre@outlook.com>
2025-06-26 18:10:43 +0000

    fix node-postgres

--- src/drizzle-schemas/conn.ts
+++ src/drizzle-schemas/conn.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/pg-core"
+import { drizzle } from "drizzle-orm/node-postgres"
 import { medicamento } from "./schema";
 
 export const dr_db = drizzle(process.env.DATABASE_URL);
