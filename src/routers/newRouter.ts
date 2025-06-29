b5d678c1dd6bb8779893b8deb4a32c0278c542a7
https-dre <diaso.andre@outlook.com>
2025-06-29 18:02:47 -0300

    feat: add new GET routes to medicine

--- src/routers/newRouter.ts
+++ src/routers/newRouter.ts
@@ -1,7 +1,48 @@
-import { MedController } from "../controllers/MedController";
+import { z } from "zod";
+import { FMedController } from "../controllers/FMedController";
 import { MedicamentoRepository } from '../repositories/mongo/MedicamentoRepository';
 
 import { FastifyInstance } from 'fastify';
+import { zMedicine } from "../models/Medicamento";
+
+const search = {
+    schema: {
+        summary: 'Search an medicine by name',
+        params: z.object({
+            name: z.string()
+        }),
+        response: {
+            200: z.array(zMedicine)
+        }
+    }
+}
+
+const getById = {
+    schema: {
+        summary: 'Get medicine by id',
+        params: z.object({
+            id: z.string()
+        }),
+        response: {
+            200: zMedicine
+        }
+    }
+}
+
+const filterByCategory = {
+    schema: {
+        summary: 'Filter medicine by category and value',
+        params: z.object({
+            category: z.string(),
+            value: z.string(),
+        }),
+        response: {
+            200: z.object({
+                data: z.array(zMedicine)
+            })
+        }
+    }
+}
 
 export const routes = async (app: FastifyInstance) => {
     app.get("/", { schema: {
@@ -9,4 +50,11 @@ export const routes = async (app: FastifyInstance) => {
     }}, (_, reply) => {
         reply.redirect('/docs');
     });
+
+    const medRepo = new MedicamentoRepository();
+    const medController = new FMedController(medRepo);
+
+    app.get('/medicine/:id', getById, medController.getById.bind(medController))
+    app.get('/medicine/search/:name', search, medController.search.bind(medController))
+    app.get('/medicine/:category/:value', filterByCategory, medController.filter.bind(medController)) 
 }
