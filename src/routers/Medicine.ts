696bace62fcd6a782e30d18e495aa636caf109dc
https-dre <diaso.andre@outlook.com>
2025-06-29 20:23:34 -0300

    rename 'newRouter.ts' to 'Medicine.ts'
    - now, each model has your own route prefix with custom router.

---
+++ src/routers/Medicine.ts
@@ -0,0 +1,126 @@
+import { z } from "zod";
+import { FMedController } from "../controllers/FMedController";
+import { MedicamentoRepository } from "../repositories/mongo/MedicamentoRepository";
+
+import { FastifyInstance } from "fastify";
+import { zMedicine } from "../models/Medicamento";
+
+const search = {
+  schema: {
+    summary: "Search an medicine by name",
+    params: z.object({
+      name: z.string(),
+    }),
+    response: {
+      200: z.object({
+        data: z.array(zMedicine),
+        dataLength: z.number()
+      }),
+    },
+  },
+};
+
+const getById = {
+  schema: {
+    summary: "Get medicine by id",
+    params: z.object({
+      id: z.string().uuid(),
+    }),
+    response: {
+      200: zMedicine,
+    },
+  },
+};
+
+const filterByScope = {
+  schema: {
+    summary: "Filter medicine by scope and value",
+    params: z.object({
+      scope: z.string(),
+      value: z.string(),
+    }),
+    response: {
+      200: z.object({
+        data: z.array(zMedicine),
+        dataLength: z.number()
+      }),
+    },
+  },
+};
+
+const postMedicine = {
+  schema: {
+    summary: "Create an medicine",
+    body: z.object({
+      med: zMedicine.omit({ id: true }),
+    }),
+    response: {
+      201: z.object({
+        id: z.string().uuid(),
+      }),
+    },
+  },
+};
+
+const deleteMedicine = {
+  schema: {
+    summary: "Delete an medicine by id",
+    params: z.object({
+      id: z.string().uuid(),
+    }),
+    response: {
+      204: z.null(),
+    },
+  },
+};
+
+export const routes = async (app: FastifyInstance) => {
+  app.get(
+    "/",
+    {
+      schema: { summary: "Redirect to /docs" },
+    },
+    (_, reply) => {
+      reply.redirect("/docs");
+    }
+  );
+
+  const medRepo = new MedicamentoRepository();
+  const medController = new FMedController(medRepo);
+
+  // Grupo de rotas com prefixo /medicine
+  app.register(
+    async (medicineRoutes) => {
+      medicineRoutes.get(
+        "/:id",
+        getById,
+        medController.getById.bind(medController)
+      );
+
+      medicineRoutes.get(
+        "/search/:name",
+        search,
+        medController.search.bind(medController)
+      );
+
+      medicineRoutes.get(
+        "/:scope/:value",
+        filterByScope,
+        medController.filter.bind(medController)
+      );
+
+      medicineRoutes.post(
+        "/",
+        postMedicine,
+        medController.save.bind(medController)
+      );
+
+      medicineRoutes.delete(
+        "/:id",
+        deleteMedicine,
+        medController.deleteById.bind(medController)
+      );
+    },
+    { prefix: "/medicine" }
+  );
+};
