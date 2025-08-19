fa77fe79fc9ae88d3752e5cfe61ee31c068c6064
André Dias <diaso.andre@outlook.com>
2025-08-19 12:11:47 +0000

    feat: add user repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,16 +1,24 @@
 import { Medicamento, MedicineImage } from "../models/Medicamento";
+import { User } from "../models/User";
 
 export interface IMedRepository {
-    findByNumRegistro(registro : string) : Promise<Medicamento | null>;
-    save(med : Medicamento) : Promise<Medicamento>;
-    updateByNumRegistro(med : any, num : string) : Promise<any>;
-    update(update: any, id: string): Promise<void>;
-    findById(id : string): Promise<Medicamento | null>;
-    getAll() : Promise<Medicamento[]>;
-    delete( Id : string) : Promise<void>;
-    deleteByNumRegistro( NumProcesso : string) : Promise<void>
-    searchByName(name : string) : Promise<Medicamento[]>;
-    filter(category: string, value: string, page: 
-        number, limit: number): Promise<Medicamento[]>;
-    insertImage(id: string, image: MedicineImage): Promise<void>;
+  findByNumRegistro(registro: string): Promise<Medicamento | null>;
+  save(med: Medicamento): Promise<Medicamento>;
+  updateByNumRegistro(med: any, num: string): Promise<any>;
+  update(update: any, id: string): Promise<void>;
+  findById(id: string): Promise<Medicamento | null>;
+  getAll(): Promise<Medicamento[]>;
+  delete(Id: string): Promise<void>;
+  deleteByNumRegistro(NumProcesso: string): Promise<void>
+  searchByName(name: string): Promise<Medicamento[]>;
+  filter(category: string, value: string, page:
+    number, limit: number): Promise<Medicamento[]>;
+  insertImage(id: string, image: MedicineImage): Promise<void>;
+}
+
+export interface IUserRepository {
+  save(data: Omit<User, "id">): Promise<User>;
+  delete(id: string): Promise<void>;
+  findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
