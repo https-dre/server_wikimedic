5999f3740158df7fcffb35b12f0880d6fcee9b18
André Dias <diaso.andre@outlook.com>
2025-08-19 11:28:13 +0000

    feat(environment): add function to check environment vars

---
+++ src/env-config.ts
@@ -0,0 +1,26 @@
+import { logger } from "./logger";
+
+const EnvConfig = new Map<string, string>(
+	Object.entries(process.env).filter(
+		(entry): entry is [string, string] => entry[1] !== undefined
+	)
+);
+
+const vars = [
+	"MONGO_URL",
+  "AWS_REGION",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY"
+]
+
+export const verify_env = () => {
+	logger.info("Checking environment...");
+	const missingVars = vars.filter((v) => !EnvConfig.has(v));
+	if (missingVars.length) {
+		logger.fatal(`Missing: ${missingVars.join(", ")}`);
+		process.exit(1);
+	}
+	logger.info("Environment ok!")
+}
+
+export default EnvConfig;
