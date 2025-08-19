5856d2a09be094cef9b4b781f290bbc38b2766af
André Dias <diaso.andre@outlook.com>
2025-08-19 13:46:57 +0000

    feat: add user service implementation

---
+++ src/services/user-service.ts
@@ -0,0 +1,60 @@
+import { z } from "zod";
+import { BadResponse } from "../error-handler";
+import { User } from "../models/User";
+import { JwtProvider } from "../providers/crypto-provider";
+import { IUserRepository } from "../repositories";
+
+export class UserService {
+  constructor(private repository: IUserRepository, public jwt: JwtProvider) {};
+
+  async saveUser(data: Omit<User, "id">) {
+    if(!await this.repository.findByEmail(data.email))
+      throw new BadResponse("E-mail já cadastrado.");
+
+    if(data.password.length < 8) {
+      throw new BadResponse("A senha deve ter mais de 8 caracteres.");
+    }
+    await this.repository.save(data);
+  }
+
+  async deleteUser(id: string) {
+    if(!await this.repository.findById(id))
+      throw new BadResponse("Usuário não encontrado.", 404);
+    await this.repository.delete(id);
+  }
+
+  async updateById(id: string, updatedFields: Partial<Omit<User, "id">>) {
+    if(!await this.repository.findById(id))
+      throw new BadResponse("Usuário não encontrado.");
+    await this.repository.updateById(id, updatedFields);
+  }
+
+  async signByLogin(email: string, password: string, tokenAge?: "1h") {
+    const userWithEmail = await this.repository.findByEmail(email);
+    if(!userWithEmail)
+      throw new BadResponse("E-mail não encontrado.", 404);
+
+    if(!(userWithEmail.password == password))
+      throw new BadResponse("Senha ou e-mail incorretos.", 403);
+
+    const token = this.jwt.generateToken({ email }, tokenAge);
+    return token
+  }
+
+  async checkToken(token: string) {
+    try {
+      const payload = this.jwt.verifyToken(token) as { email: string };
+      if(!(await this.repository.findByEmail(payload.email)))
+        throw new BadResponse("E-mail não registrado, sessão inválida.", 403);
+      
+    } catch (err: any) {
+      if (err.name == "TokenExpiredError") {
+        throw new BadResponse("Sessão expirou.", 403);
+      }
+      if (err.name == "JsonWebTokenError") {
+        throw new BadResponse("Sessão inválida.", 403);
+      }
+      throw err
+    }
+  }
+}
