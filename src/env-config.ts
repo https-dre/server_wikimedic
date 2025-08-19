25b236221a1a4ca405c3da67b9d90a3d60848768
https-dre <diaso.andre@outlook.com>
2025-08-19 18:22:08 -0300

    refactor: replace AWS vars with Supabase vars

--- src/env-config.ts
+++ src/env-config.ts
@@ -1,27 +1,22 @@
 import { logger } from "./logger";
 
 const EnvConfig = new Map<string, string>(
-	Object.entries(process.env).filter(
-		(entry): entry is [string, string] => entry[1] !== undefined
-	)
+  Object.entries(process.env).filter(
+    (entry): entry is [string, string] => entry[1] !== undefined
+  )
 );
 
-const vars = [
-	"MONGO_URL",
-  "AWS_REGION",
-  "AWS_ACCESS_KEY_ID",
-  "AWS_SECRET_ACCESS_KEY",
-	"AWS_BUCKET_NAME"
-]
+const vars = ["MONGO_URL", "SUPABASE_URL", "SUPABASE_KEY"];
 
 export const verify_env = () => {
-	logger.info("Checking environment...");
-	const missingVars = vars.filter((v) => !EnvConfig.has(v));
-	if (missingVars.length) {
-		logger.fatal(`Missing: ${missingVars.join(", ")}`);
-		process.exit(1);
-	}
-	logger.info("Environment ok!")
-}
+  logger.info("Checking environment...");
+  const missingVars = vars.filter((v) => !EnvConfig.has(v));
+  if (missingVars.length) {
+    logger.fatal(`Missing: ${missingVars.join(", ")}`);
+    process.exit(1);
+  }
+  logger.info("Environment ok!");
+};
 
 export default EnvConfig;
+
