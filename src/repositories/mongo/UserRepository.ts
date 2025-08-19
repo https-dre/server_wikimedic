5609e324d8fee09fc5b4794ceebbbd0aba518600
André Dias <diaso.andre@outlook.com>
2025-08-19 12:30:41 +0000

    feat: implement user repository

---
+++ src/repositories/mongo/UserRepository.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from "crypto";
+import { IUserRepository } from "..";
+import { User } from "../../models/User";
+import { mongo } from "../../data/mongoDB/conn"
+import { ObjectId } from "mongodb";
+
+export class UserRepository implements IUserRepository {
+  async save(data: Omit<User, "id">): Promise<void> {
+    const users = mongo.db.collection("user");
+    await users.insertOne({
+      _id: new ObjectId(randomUUID()),
+      ...data
+    });
+  }
+  async delete(id: string): Promise<void> {
+    const users = mongo.db.collection("user");
+    await users.deleteOne({ _id: new ObjectId(id) });
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    const users = mongo.db.collection("user");
+    const result = await users.findOne<User & { _id: ObjectId }>({ email });
+    if (!result) return null;
+    return result;
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const users = mongo.db.collection("user");
+    const result = await users.findOne<User & { _id: ObjectId }>({ _id: new ObjectId(id)});
+    if (!result) return null;
+    return result;
+  }
+
+  async updateById(id: string, update: Partial<Omit<User, "id">>): Promise<void> {
+    const users = mongo.db.collection("user");
+    await users.updateOne({ _id: new ObjectId(id)}, { $set: update });
+  }
+}
